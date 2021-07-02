80304f327ebeb6eaa99161e1f58a1e3ee1a544a3
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-03 02:28:39 +0300

    add revision to the functionality to call the function from document.ready

--- src/main.js
+++ src/main.js
@@ -4,31 +4,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency';
 
-let getElements = (response) => {
-  let results = 0;
-  for (let choice of response) {
-    if (response.conversion_rate.AED) {
-      results = response.conversion_rate.AED * choice;
-      return results;
-    }
-  }  
-  
-};
-
-async function makeAPICall(input, choice) {
-  const response = await Currency.get(input, choice);
-  getElements(response);
-}
 
 $(document).ready(function() {
   $("#convert-currency").submit(function(event) {
     event.preventDefault;
 
-    $('#output').text('');
-
     let input = $("#us-dollars").val();
     let choice = $("#other-currency").val();
     
-    makeAPICall(input, choice);
+    let response = await Currency.get(input)
+
+    .then(function(results) {
+        if (results instanceof Error) {
+          throw Error("err");
+        }
+        let outputAmt = 0;
+        if (choice === "AED") {
+          outputAmt = response.conversion_rates.AED * input;
+        } else if (choice === "EUR") {
+          outputAmt = response.conversion_rates.EUR * input;
+        }
+    })
   });
 });
