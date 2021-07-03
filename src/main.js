aad9be9ceda70936ad4e3ca322362c26733dc19d
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-03 16:20:44 +0300

    removed .env file

--- src/main.js
+++ src/main.js
@@ -4,42 +4,40 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency';
 
+let getElements = function(response) {
+  let outputAmt = 0;
+  if (response.conversion_rates.AED) {
+    outputAmt = response.conversion_rates.AED * response;
+  } else if (response.conversion_rates.EUR) {
+    outputAmt = response.conversion_rates.EUR * response;
+  } else if (response.conversion_rates.JPY) {
+    outputAmt = response.conversion_rates.JPY * response;
+  } else if (response.conversion_rates.INR) {
+    outputAmt = response.conversion_rates.INR * response;
+  } else if (response.conversion_rates.GBP) {
+    outputAmt = response.conversion_rates.GBP * response;
+  } else {
+    $("#noCurrency").text();
+  }
+  $("#output").html(outputAmt);
+};
+
+
 async function makeApiCall(input, choice) {
   const response = await Currency.get(input, choice);
+  getElements(response);
 }
 
 $(document).ready(function() {
   $("#convert-currency").submit(function(event) {
-    event.preventDefault;
+    event.preventDefault();
 
     let input = $("#us-dollars").val();
     let choice = $("#other-currency").val();
     
-    makeApiCall(input, choice)
-
-      .then(function(results) {
-        if (response instanceof Error) {
-          throw Error("err");
-        }
-        let outputAmt = 0;
-        if (choice === "AED") {
-          outputAmt = response.conversion_rates.AED * input;
-        } else if (choice === "EUR") {
-          outputAmt = response.conversion_rates.EUR * input;
-        } else if (choice === "JPY") {
-          outputAmt = response.conversion_rates.JPY * input;
-        } else if (choice === "INR") {
-          outputAmt = response.conversion_rates.INR * input;
-        } else if (choice === "GBP") {
-          outputAmt = response.conversion_rates.GBP * input;
-        } else {
-          $("#noCurrency").text();
-        }
-        $("#output").html(outputAmt);
-      })
-      .catch(function(error) {
-        $('#showErrors').text(`There was an error processing your request: ${error}`);
-      });
+    console.log('')
+    console.log('input', input);
+    makeApiCall(input, choice);
   });
 });
 
