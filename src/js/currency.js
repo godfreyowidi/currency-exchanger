547e9d5b5fe1a68d05ed040c55ea6dba8ae92f19
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-07-11 17:00:22 +0300

    add revision to getElements function

--- src/js/currency.js
+++ src/js/currency.js
@@ -1,6 +1,6 @@
 export default class Currency {  
-  static getConvert(input) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${input}`)
+  static getConvert() {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
--- src/main.js
+++ src/main.js
@@ -4,30 +4,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/currency';
 
-function getElements(response) {
+function getElements(response, selectedCurr, dollars) {
+  //console.log('in getElements, response', response);
   let results;
+  //console.log('in getElements, response.conversion_rates', response.conversion_rates);
   if (response.conversion_rates) {
-    results = response.conversion_rates.results;
+    results = response.conversion_rates;
+    //console.log("res", results);
+    if (selectedCurr === 'AED') {
+      $("#showAED").text(`United Arab Emirate: ${results.AED * dollars}`); 
+    } else if (selectedCurr === "EUR") {
+      $("#showEUR").text(`European EURO: ${results.EUR * dollars}`);
+    } else if (selectedCurr === "JPY") {
+      $("#showJPY").text(`Japanese Yen: ${results.JPY * dollars}`);
+    } else if (selectedCurr === "INR") {
+      $("#showINR").text(`Indian Rupee: ${results.INR * dollars}`);
+    } else if (selectedCurr === "GBP") {
+      $("#showGBP").text(`British Pound: ${results.GBP * dollars}`);
+    } else {
+      $("#noNum").show();
+    }
+
   } else {
-    results = response.results;
+    results = results.conversion_rates;
   }
 }
 
-for (input in results) {
-  if (results.conversion_rates === "AED") {
-    $("#showAED").text(`United Arab Emirate: ${results.conversion_rates.AED}`)
-  }
-}
+
 $(document).ready(function() {
-  $("#convert-currecny").submit(function(event) {
+  $("#convert-currency").submit(function(event) {
     event.preventDefault();
-
+    $("#us-dollars").val('');
     const input = $("#other-currency").val();
-    const usDollar = parseInt($("#us-dollars")).val();
-
+    const usDollar = parseInt($("#us-dollars").val());
+  
     Currency.getConvert(input)
       .then(function(response) {
-        getElements(response);
+        getElements(response, input, usDollar);
+        if(response instanceof Error) {
+          throw Error('error to get to res');
+        }
       });  
   });
 });
